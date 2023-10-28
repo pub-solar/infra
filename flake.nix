e0c6530d97b12baf9ec7378fc0d58f438e010590
Akshay Mankar <itsakshaymankar@gmail.com>
2023-10-28 12:01:48 +0200

    Disable Password authentication in SSH

--- flake.nix
+++ flake.nix
@@ -59,6 +59,7 @@
               virtualisation.docker.enable = true;
               services.openssh.enable = true;
               services.openssh.settings.PermitRootLogin = "prohibit-password";
+              services.openssh.settings.PasswordAuthentication = false;
             };
 
             # NixOS specific configuration
