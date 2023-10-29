505602bd73d576c1e67b85a9a36bc0a736a4b277
teutat3s <teutates@mailbox.org>
2023-10-29 23:04:33 +0100

    fix: set max body size to 1G for forgejo

--- hosts/nachtigall/apps/forgejo.nix
+++ hosts/nachtigall/apps/forgejo.nix
@@ -25,7 +25,12 @@
         return 302 /user/oauth2/keycloak;
     '';
 
-    locations."/".proxyPass = "http://127.0.0.1:3000";
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:3000";
+      extraConfig = ''
+        client_max_body_size 1G;
+      '';
+    };
   };
 
   services.gitea = {
