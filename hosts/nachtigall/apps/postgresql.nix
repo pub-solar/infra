94c7b6d7db5256a0128d27b514e64db90ec07336
teutat3s <teutates@mailbox.org>
2023-10-29 20:53:16 +0100

    fix: postgresql service should always wait for ZFS mount

--- hosts/nachtigall/apps/postgresql.nix
+++ hosts/nachtigall/apps/postgresql.nix
@@ -2,4 +2,12 @@
 
 {
   services.postgresql.enable = true;
+  systemd.services.postgresql = {
+    after = [
+      "var-lib-postgresql.mount"
+    ];
+    requisite = [
+      "var-lib-postgresql.mount"
+    ];
+  };
 }
