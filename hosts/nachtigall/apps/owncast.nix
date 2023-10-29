f48782e98722e4b46e318b5172beeec6c2655ee7
teutat3s <teutates@mailbox.org>
2023-10-29 03:35:35 +0100

    feat: owncast initial commit
    https://owncast.online/

---
+++ hosts/nachtigall/apps/owncast.nix
@@ -0,0 +1,30 @@
+{
+  flake,
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  services.nginx.virtualHosts."stream.pub.solar" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/".proxyPass = "http://127.0.0.1:5000";
+  };
+
+  # Owncast
+  services.owncast = {
+    enable = true;
+    user = "owncast";
+    group = "owncast";
+    # The directory where owncast stores its data files.
+    dataDir = "/var/lib/owncast";
+    # Open the appropriate ports in the firewall for owncast.
+    openFirewall = true;
+    # The IP address to bind the owncast web server to.
+    listen = "127.0.0.1";
+    # TCP port where owncast rtmp service listens.
+    rtmp-port = 1935;
+    # TCP port where owncast web-gui listens.
+    port = 5000;
+  };
+}
