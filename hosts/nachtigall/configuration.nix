9c1d19d49f03efb3cbc0e99dcdbe54f67aa7be1e
teutat3s <teutates@mailbox.org>
2023-11-09 03:20:48 +0100

    nachtigall: move SSH private key from user to host

--- hosts/nachtigall/configuration.nix
+++ hosts/nachtigall/configuration.nix
@@ -38,6 +38,14 @@
   # https://nixos.wiki/wiki/ZFS#declarative_mounting_of_ZFS_datasets
   systemd.services.zfs-mount.enable = false;
 
+  # Declarative SSH private key
+  age.secrets."nachtigall-root-ssh-key" = {
+    file = "${flake.self}/secrets/nachtigall-root-ssh-key.age";
+    path = "/root/.ssh/id_ed25519";
+    mode = "400";
+    owner = "root";
+  };
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
