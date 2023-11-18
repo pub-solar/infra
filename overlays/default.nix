eeb15c93fccc09d0f67be6a63ca05589c31cbcce
teutat3s <teutates@mailbox.org>
2023-11-18 18:45:58 +0100

    feat: update forgejo to version 1.20.5-0
    https://codeberg.org/forgejo/forgejo/src/branch/forgejo/RELEASE-NOTES.md#1-20-5-0

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,6 +9,7 @@
         nixpkgs.overlays = [
           (final: prev: {
             mastodon = inputs.mastodon-fork.legacyPackages.${prev.system}.mastodon;
+            forgejo = inputs.unstable.legacyPackages.${prev.system}.forgejo;
             forgejo-actions-runner = inputs.unstable.legacyPackages.${prev.system}.forgejo-actions-runner;
 
             mediawiki = inputs.unstable.legacyPackages.${prev.system}.mediawiki;
