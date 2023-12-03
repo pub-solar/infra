630723516dd2ff558ed8ecd215733618a8b1d71d
teutat3s <teutates@mailbox.org>
2023-12-03 14:50:19 +0100

    fix: remove DNS ttl until we need it again

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -38,13 +38,11 @@ resource "namecheap_domain_records" "pub-solar" {
     hostname = "obs-portal"
     type = "A"
     address = "80.71.153.210"
-    ttl = 60
   }
   record {
     hostname = "vpn"
     type = "A"
     address = "80.71.153.210"
-    ttl = 60
   }
   record {
     hostname = "cache"
@@ -81,7 +79,6 @@ resource "namecheap_domain_records" "pub-solar" {
     hostname = "turn"
     type = "A"
     address = "138.201.80.102"
-    ttl = 300
   }
   record {
     hostname = "grafana"
@@ -92,7 +89,6 @@ resource "namecheap_domain_records" "pub-solar" {
     hostname = "hpb"
     type = "A"
     address = "80.71.153.239"
-    ttl = 60
   }
   record {
     hostname = "files"
