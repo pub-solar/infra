e6177069ab9d4cbb182eb8d037227a6c9e7e51a8
teutat3s <teutates@mailbox.org>
2023-12-14 00:49:21 +0100

    fix(security): pull in forgejo 1.20.6-1 early
    https://codeberg.org/forgejo/forgejo/src/branch/forgejo/RELEASE-NOTES.md#1206-1
    
    https://github.com/NixOS/nixpkgs/pull/274026
    https://nixpk.gs/pr-tracker.html?pr=274026
    
    • Added input 'release-2311':
        'github:nixos/nixpkgs/c15f414581b4eb4113eed52ed303a1e62771fb6f' (2023-12-13)

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,8 +7,15 @@
     nixosModules = rec {
       overlays = ({ ... }: {
         nixpkgs.overlays = [
-          (final: prev: {
+          (final: prev:
+          let
+            release-2311 = import inputs.release-2311 {
+              system = prev.system;
+            };
+          in
+          {
             element-themes = prev.callPackage ./pkgs/element-themes { inherit (inputs) element-themes; };
+            forgejo = release-2311.forgejo;
           })
         ];
       });
