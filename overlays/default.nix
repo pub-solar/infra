7cc3a261edb728d05b2c0890514793c5d3b1ed5f
teutat3s <teutates@mailbox.org>
2023-12-18 12:39:04 +0100

    chore: update flake inputs home-manager, nixpkgs,
    remove temporary input release-2311
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/6761b8188b860f374b457eddfdb05c82eef9752f' (2023-12-10)
      → 'github:nix-community/home-manager/1488651d02c1a7a15e284210f0d380a62d8d8cef' (2023-12-17)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/cf28ee258fd5f9a52de6b9865cdb93a1f96d09b7' (2023-12-12)
      → 'github:nixos/nixpkgs/1e2e384c5b7c50dbf8e9c441a9e58d85f408b01f' (2023-12-17)
    • Removed input 'release-2311'

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,15 +9,13 @@
         nixpkgs.overlays = [
           (final: prev:
           let
-            release-2311 = import inputs.release-2311 {
+            unstable = import inputs.unstable {
               system = prev.system;
             };
           in
           {
             element-themes = prev.callPackage ./pkgs/element-themes { inherit (inputs) element-themes; };
-            forgejo = release-2311.forgejo;
           })
-          (import ./keycloak.nix)
         ];
       });
     };
--- overlays/keycloak.nix
+++
@@ -1,9 +0,0 @@
-final: prev: {
-  keycloak = prev.keycloak.overrideAttrs (oldAttrs: rec {
-    version = "999.0.0-SNAPSHOT";
-    src = prev.fetchzip {
-      url = "https://github.com/keycloak/keycloak/releases/download/nightly/keycloak-${version}.zip";
-      hash = "sha256-2rOhBmPXQy6HK3CtG/7E2EUK5zEWrJtSgBg6AMw2q3E=";
-    };
-  });
-}
