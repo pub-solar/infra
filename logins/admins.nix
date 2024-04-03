147ed44b9af3e57b0208233b9960a6bd25c05801
teutat3s <teutates@mailbox.org>
2024-04-03 21:06:55 +0200

    wireguard: add dumpyourvms

--- logins/admins.nix
+++ logins/admins.nix
@@ -23,6 +23,10 @@
         publicKey = "NNb7T8Jmn+V2dTZ8T6Fcq7hGomHGDckKoV3kK2oAhSE=";
         allowedIPs = [ "10.7.6.200/32" "fd00:fae:fae:fae:fae:200::/96" ];
       }
+      { # dumpyourvms
+        publicKey = "3UrVLQrwXnPAVXPiTAd7eM3fZYxnFSYgKAGpNMUwnUk=";
+        allowedIPs = [ "10.7.6.201/32" "fd00:fae:fae:fae:fae:201::/96" ];
+      }
     ];
   };
 
