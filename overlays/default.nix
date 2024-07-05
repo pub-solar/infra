f8f67b8908ae7612631ee806b99a078b0efac41f
teutat3s <teutates@mailbox.org>
2024-07-05 11:56:50 +0200

    security: update mastodon to 4.2.10
    https://github.com/mastodon/mastodon/releases/tag/v4.2.10
    * Fix incorrect permission checking on multiple API endpoints (GHSA-58x8-3qxw-6hm7)
    * Fix incorrect authorship checking when processing some activities (CVE-2024-37903, GHSA-xjvf-fm67-4qc3)
    * Fix ongoing streaming sessions not being invalidated when application tokens get revoked (GHSA-vp5r-5pgw-jwqx)

--- overlays/default.nix
+++ overlays/default.nix
@@ -16,6 +16,22 @@
                 element-stickerpicker = prev.callPackage ./pkgs/element-stickerpicker {
                   inherit (inputs) element-stickers maunium-stickerpicker;
                 };
+                mastodon = prev.mastodon.override {
+                  version = "4.2.10";
+                  patches = [
+                    (final.fetchpatch {
+                      url = "https://github.com/mastodon/mastodon/compare/v4.2.9...v4.2.10.patch";
+                      hash = "sha256-268iq+2E5OOlhaJE1u5q7AFPdsloXpZCEXoyRMLtRys=";
+                    })
+                  ];
+
+                  # Mastodon has been upgraded on master, the backport is still
+                  # in progress. This is a temporary hack until the backport
+                  # makes it to the branch nixos-24.05.
+                  # https://github.com/NixOS/nixpkgs/pull/324587
+                  # https://nixpk.gs/pr-tracker.html?pr=324587
+                  gemset = import "${inputs.nixpkgs-head.sourceInfo.outPath}/pkgs/servers/mastodon/gemset.nix";
+                };
               }
             )
           ];
