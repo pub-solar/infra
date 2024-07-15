26e96dfac5dc12e0a6756595fa3de841730d1c7e
teutat3s <teutates@mailbox.org>
2024-07-15 18:51:10 +0200

    mediawiki: update to v1.42.1

--- modules/mediawiki/default.nix
+++ modules/mediawiki/default.nix
@@ -211,7 +211,7 @@ in
       backend = "docker";
 
       containers."mediawiki" = {
-        image = "git.pub.solar/pub-solar/mediawiki-oidc-docker:1.41.1";
+        image = "git.pub.solar/pub-solar/mediawiki-oidc-docker:1.42.1";
         user = "1000:${builtins.toString gid}";
         autoStart = true;
 
