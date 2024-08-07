30d18c725a1b19c24cf949f0937f8d0b8cf71a0b
teutat3s <teutates@mailbox.org>
2024-08-07 10:09:37 +0200

    revert: overlay for element-web and matrix-synapse
    both packages now have the desired version available in nixos-24.05

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,15 +10,12 @@
               final: prev:
               let
                 unstable = import inputs.unstable { system = prev.system; };
-                nixpkgs-331083-331168 = import inputs.nixpkgs-331083-331168 { system = prev.system; };
               in
               {
                 element-themes = prev.callPackage ./pkgs/element-themes { inherit (inputs) element-themes; };
                 element-stickerpicker = prev.callPackage ./pkgs/element-stickerpicker {
                   inherit (inputs) element-stickers maunium-stickerpicker;
                 };
-                element-web = nixpkgs-331083-331168.element-web;
-                matrix-synapse-unwrapped = nixpkgs-331083-331168.matrix-synapse-unwrapped;
               }
             )
           ];
