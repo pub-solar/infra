4626fd85c0b6d84f6c667095d3df8e3e0a7f87fe
teutat3s <teutates@mailbox.org>
2024-08-28 17:10:17 +0200

    mediawiki: add backups to garage bucket + storagebox
    Restic backups to garage S3 bucket nachtigall-backups
    https://garagehq.deuxfleurs.fr/documentation/connect/backup/#restic

--- hosts/nachtigall/backups.nix
+++ hosts/nachtigall/backups.nix
@@ -1,4 +1,4 @@
-{ flake, ... }:
+{ config, flake, ... }:
 {
   age.secrets."restic-repo-droppie" = {
     file = "${flake.self}/secrets/restic-repo-droppie.age";
@@ -9,5 +9,26 @@
     file = "${flake.self}/secrets/restic-repo-storagebox.age";
     mode = "400";
     owner = "root";
+  };
+  age.secrets.restic-repo-garage-nachtigall = {
+    file = "${flake.self}/secrets/restic-repo-garage-nachtigall.age";
+    mode = "400";
+    owner = "root";
+  };
+  age.secrets.restic-repo-garage-nachtigall-env = {
+    file = "${flake.self}/secrets/restic-repo-garage-nachtigall-env.age";
+    mode = "400";
+    owner = "root";
+  };
+
+  pub-solar-os.backups.repos.storagebox = {
+    passwordFile = config.age.secrets."restic-repo-storagebox".path;
+    repository = "sftp:u377325@u377325.your-storagebox.de:/backups";
+  };
+
+  pub-solar-os.backups.repos.garage = {
+    passwordFile = config.age.secrets."restic-repo-garage-nachtigall".path;
+    environmentFile = config.age.secrets."restic-repo-garage-nachtigall-env".path;
+    repository = "s3:https://buckets.pub.solar/nachtigall-backups";
   };
 }
