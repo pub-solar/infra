6b6e2d9f6e42840ac543136ee433915b5b1269ce
teutat3s <teutates@mailbox.org>
2025-02-09 18:50:35 +0100

    matrix-authentication-service: use 0.13.0 from nixos-unstable

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,10 +10,9 @@
               final: prev:
               let
                 unstable = import inputs.unstable { system = prev.system; };
-                fork = import inputs.fork { system = prev.system; };
               in
               {
-                matrix-authentication-service = fork.matrix-authentication-service;
+                matrix-authentication-service = unstable.matrix-authentication-service;
                 element-themes = prev.callPackage ./pkgs/element-themes { inherit (inputs) element-themes; };
                 element-stickerpicker = prev.callPackage ./pkgs/element-stickerpicker {
                   inherit (inputs) element-stickers maunium-stickerpicker;
