398aad88b9730253b7312d406cac1d75ad7f7586
teutat3s <teutates@mailbox.org>
2025-04-28 21:38:03 +0200

    prometheus: use wireguard domain instead of IP

--- modules/prometheus/default.nix
+++ modules/prometheus/default.nix
@@ -219,7 +219,7 @@ in
         job_name = "alertmanager";
         static_configs = [
           {
-            targets = [ "10.7.6.5:${toString config.services.prometheus.alertmanager.port}" ];
+            targets = [ "trinkgenossin.wg.${config.pub-solar-os.networking.domain}:${toString config.services.prometheus.alertmanager.port}" ];
             labels = {
               instance = "trinkgenossin";
             };
