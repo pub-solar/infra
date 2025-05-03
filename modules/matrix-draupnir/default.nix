d1bf03eed16438ae9e8e4fdfe9c5304eaa4c6293
teutat3s <teutates@mailbox.org>
2025-05-03 16:15:51 +0200

    matrix-draupnir: refactor to pub-solar-os module

--- modules/matrix-draupnir/default.nix
+++ modules/matrix-draupnir/default.nix
@@ -1,24 +1,40 @@
-{ config, flake, ... }:
+{
+  config,
+  flake,
+  lib,
+  ...
+}:
 {
   imports = [ "${flake.inputs.nixpkgs-draupnir}/nixos/modules/services/matrix/draupnir.nix" ];
 
   disabledModules = [ "services/matrix/draupnir.nix" ];
 
-  age.secrets."matrix-draupnir-access-token" = {
-    file = "${flake.self}/secrets/matrix-draupnir-access-token.age";
-    mode = "640";
-    owner = "root";
-    group = "draupnir";
+  options.pub-solar-os.matrix-draupnir = with lib; {
+    enable = mkEnableOption "Enable matrix-draupnir moderation bot";
+
+    homeserver-url = mkOption {
+      description = "Matrix homeserver URL";
+      type = types.str;
+      example = "http://127.0.0.1:8008";
+    };
+
+    access-token-file = mkOption {
+      description = "Path to access token file";
+      type = types.str;
+    };
   };
 
-  services.draupnir = {
-    enable = true;
-    accessTokenFile = config.age.secrets.matrix-draupnir-access-token.path;
-    # https://github.com/the-draupnir-project/Draupnir/blob/main/config/default.yaml
-    settings = {
-      homeserverUrl = "http://127.0.0.1:8008";
-      managementRoom = "#moderators:${config.pub-solar-os.networking.domain}";
-      protectAllJoinedRooms = true;
+  config = lib.mkIf config.pub-solar-os.matrix-draupnir.enable {
+
+    services.draupnir = {
+      enable = true;
+      accessTokenFile = config.pub-solar-os.draupnir.access-token-file;
+      # https://github.com/the-draupnir-project/Draupnir/blob/main/config/default.yaml
+      homeserverUrl = config.pub-solar-os.draupnir.homeserver-url;
+      settings = {
+        managementRoom = "#moderators:${config.pub-solar-os.networking.domain}";
+        protectAllJoinedRooms = true;
+      };
     };
   };
 }
