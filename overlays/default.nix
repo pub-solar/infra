e2c4b61f1648bb993e2800964d7c0a3b22b2b23b
teutat3s <teutates@mailbox.org>
2025-05-22 01:04:29 +0200

    matrix: update draupnir to latest beta, switch to
    synapse-http-antispam module, add option for synapse-http-antispam secret

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,6 +10,7 @@
               final: prev:
               let
                 unstable = import inputs.unstable { system = prev.system; };
+                nixpkgs-draupnir = import inputs.nixpkgs-draupnir { system = prev.system; };
               in
               {
                 # Patch to always use port 443 in redirects from http -> https
@@ -17,11 +18,14 @@
                 codeberg-pages = unstable.codeberg-pages.overrideAttrs (oldAttrs: {
                   patches = [ ./0001-workaround-don-t-change-ssl-port-in-redirect.patch ];
                 });
-                draupnir = unstable.draupnir;
+                # want draupnir v2.3.0-main-39e452
+                draupnir = nixpkgs-draupnir.draupnir;
                 element-themes = prev.callPackage ./pkgs/element-themes { inherit (inputs) element-themes; };
                 element-stickerpicker = prev.callPackage ./pkgs/element-stickerpicker {
                   inherit (inputs) element-stickers maunium-stickerpicker;
                 };
+                # want synapse-http-antispam version 0.4.0
+                synapse-http-antispam = prev.callPackage ./pkgs/synapse-http-antispam { };
                 nextcloud-skeleton = prev.callPackage ./pkgs/nextcloud-skeleton { };
                 delete-pubsolar-id = prev.callPackage ./pkgs/delete-pubsolar-id { };
               }
