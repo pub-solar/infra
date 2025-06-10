f81243bee7febd22ef5090911c8f003f7d99a9c5
Hendrik Sokolowski <hensoko@gssws.de>
2025-06-11 01:38:05 +0200

    tankstelle: remove ipv4

--- hosts/tankstelle/networking.nix
+++ hosts/tankstelle/networking.nix
@@ -3,10 +3,6 @@
   networking = {
     hostName = "tankstelle";
     enableIPv6 = true;
-    defaultGateway = {
-      address = "80.244.242.1";
-      interface = "enp1s0";
-    };
     defaultGateway6 = {
       address = "2001:4d88:1ffa:26::1";
       interface = "enp1s0";
@@ -16,12 +12,6 @@
       "2620:fe::9"
     ];
     interfaces."enp1s0" = {
-      ipv4.addresses = [
-        {
-          address = "80.244.242.5";
-          prefixLength = 29;
-        }
-      ];
       ipv6.addresses = [
         {
           address = "2001:4d88:1ffa:26::5";
