cdc382881497d744cfc0c936a0d4c9a538d53b33
Hendrik Sokolowski <hensoko@gssws.de>
2025-06-14 23:45:39 +0200

    treefmt

--- hosts/tankstelle/networking.nix
+++ hosts/tankstelle/networking.nix
@@ -12,10 +12,10 @@
       interface = "enp1s0";
     };
     nameservers = [
-      "95.129.51.51"    # hosting.de DC resolver
-      "80.244.244.244"  # hosting.de DC resolver
-      "2620:fe::fe"     # cloud9 primary DNS
-      "2620:fe::9"      # cloud9 secondary DNS
+      "95.129.51.51" # hosting.de DC resolver
+      "80.244.244.244" # hosting.de DC resolver
+      "2620:fe::fe" # cloud9 primary DNS
+      "2620:fe::9" # cloud9 secondary DNS
     ];
     interfaces."enp1s0" = {
       ipv4.addresses = [
