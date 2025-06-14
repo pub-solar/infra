20ff38f0b3dbd58797bf7a4bd23e37ed65c55c31
teutat3s <teutates@mailbox.org>
2025-06-14 19:43:28 +0200

    tankstelle: override nameservers to use IPv6 only

--- hosts/tankstelle/networking.nix
+++ hosts/tankstelle/networking.nix
@@ -1,15 +1,22 @@
 { ... }:
 {
+  services.resolved.fallbackDns = [
+    "2001:678:e68:f000::#dot.ffmuc.net"
+    "2001:678:ed0:f000::#dot.ffmuc.net"
+  ];
   networking = {
     hostName = "tankstelle";
+    useDHCP = false;
     enableIPv6 = true;
     defaultGateway6 = {
       address = "2001:4d88:1ffa:26::1";
       interface = "enp1s0";
     };
     nameservers = [
-      "2620:fe::fe"
-      "2620:fe::9"
+      "2a0f:fc80::#dns0.eu"
+      "2a0f:fc81::#dns0.eu"
+      "2620:fe::fe#dns.quad9.net"
+      "2620:fe::9#dns.quad9.net"
     ];
     interfaces."enp1s0" = {
       ipv6.addresses = [
