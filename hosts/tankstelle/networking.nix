58573f1e87815a1b61ae1fea074b0060477f1d33
teutat3s <teutates@mailbox.org>
2025-06-15 16:56:14 +0200

    tankstelle: remove nameservers, use default from
    core/networking/networking.nix

--- hosts/tankstelle/networking.nix
+++ hosts/tankstelle/networking.nix
@@ -11,13 +11,8 @@
       address = "2001:4d88:1ffa:26::1";
       interface = "enp1s0";
     };
-    nameservers = [
-      "95.129.51.51" # hosting.de DC resolver
-      "80.244.244.244" # hosting.de DC resolver
-      "2620:fe::fe" # cloud9 primary DNS
-      "2620:fe::9" # cloud9 secondary DNS
-    ];
     interfaces."enp1s0" = {
+      useDHCP = false;
       ipv4.addresses = [
         {
           address = "80.244.242.6";
