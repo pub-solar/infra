7ba891236124a70ddd80c1226d631ce72d2f6f9f
teutat3s <teutates@mailbox.org>
2025-09-03 20:00:33 +0200

    tests: wait for rspamd socket file

--- tests/keycloak.nix
+++ tests/keycloak.nix
@@ -80,6 +80,7 @@ in
       mail_server.wait_for_unit("dovecot2.service")
       mail_server.wait_for_unit("postfix.service")
       mail_server.wait_for_unit("rspamd.service")
+      mail_server.wait_for_file("/run/rspamd/rspamd-milter.sock")
 
       mail_server.wait_until_succeeds("curl http://mail.test.pub.solar/")
       puppeteer_succeed('page.locator("input[type=submit][value=Register]").click()')
