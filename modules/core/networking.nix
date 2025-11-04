e5eb4d68ea43516dfd9aa2109e8a1ecd598a7935
b12f <git@benjaminbaedorf.eu>
2025-11-04 23:04:01 +0100

    core: harden SSH settings
    This removes deprecated MAC ciphers.

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -50,26 +50,8 @@
       enable = true;
       openFirewall = lib.mkDefault false;
       settings = {
-        PermitRootLogin = "prohibit-password";
+        PermitRootLogin = "no";
         PasswordAuthentication = false;
-        # Add back openssh MACs that got removed from defaults
-        # for backwards compatibility
-        #
-        # NixOS default openssh MACs have changed to use "encrypt-then-mac" only.
-        # This breaks compatibilty with clients that do not offer these MACs. For
-        # compatibility reasons, we add back the old defaults.
-        # See: https://github.com/NixOS/nixpkgs/pull/231165
-        #
-        # https://blog.stribik.technology/2015/01/04/secure-secure-shell.html
-        # https://infosec.mozilla.org/guidelines/openssh#modern-openssh-67
-        Macs = [
-          "hmac-sha2-512-etm@openssh.com"
-          "hmac-sha2-256-etm@openssh.com"
-          "umac-128-etm@openssh.com"
-          "hmac-sha2-512"
-          "hmac-sha2-256"
-          "umac-128@openssh.com"
-        ];
       };
     };
 
