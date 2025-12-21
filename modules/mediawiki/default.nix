a9012e1fafd3dec7a96a6145dcbeb932afbf1245
teutat3s <teutates@mailbox.org>
2025-12-21 15:37:31 +0100

    mediawiki: security update to 1.43.6

--- modules/mediawiki/default.nix
+++ modules/mediawiki/default.nix
@@ -286,7 +286,7 @@ in
       backend = "docker";
 
       containers."mediawiki" = {
-        image = "git.pub.solar/pub-solar/mediawiki-oidc-docker:1.43.5";
+        image = "git.pub.solar/pub-solar/mediawiki-oidc-docker:1.43.6";
         user = "1000:${builtins.toString gid}";
         autoStart = true;
 
