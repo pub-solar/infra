ddda24d25ea68e12b0f78632422e7d21f8923000
teutat3s <teutates@mailbox.org>
2026-05-17 19:36:09 +0200

    security(core): disable vulnerable kernel modules
    Fixes CVE-2026-43284, CVE-2026-43500

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -1,6 +1,18 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
 {
   boot.loader.grub.configurationLimit = lib.mkDefault 15;
   boot.loader.systemd-boot.configurationLimit = lib.mkDefault 15;
+
+  # Prevent CVE-2026-43284 CVE-2026-43500
+  boot.extraModprobeConfig = ''
+    install esp4 ${pkgs.coreutils}/bin/false
+    install esp6 ${pkgs.coreutils}/bin/false
+    install rxrpc ${pkgs.coreutils}/bin/false
+  '';
+  boot.blacklistedKernelModules = [
+    "esp4"
+    "esp6"
+    "rxrpc"
+  ];
 }
